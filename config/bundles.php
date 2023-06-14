df390b5691c936eef44fd73158ef5978d412f8c9
pomowunk <81287967+pomowunk@users.noreply.github.com>
2023-06-14 10:24:54 +0200

    initialize symfony/flex

---
+++ config/bundles.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    JMS\TranslationBundle\JMSTranslationBundle::class => ['all' => true],
+    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Runalyze\Bundle\GlossaryBundle\GlossaryBundle::class => ['all' => true],
+    Runalyze\Bundle\PlaygroundBundle\PlaygroundBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+];
--- src/functions.php
+++ src/functions.php
@@ -7,33 +7,39 @@
  * but this will stay until all old code smells are removed.
  */
 
-/**
- * Returns the translation for a textstring
- * @param string $text
- * @return string
- */
-function __($text)
-{
-    return gettext($text);
+if (!function_exists('__')) {
+    /**
+     * Returns the translation for a textstring
+     * @param string $text
+     * @return string
+     */
+    function __($text)
+    {
+        return gettext($text);
+    }
 }
 
-/**
- * Echo the translation for a textstring
- * @param string $text
- */
-function _e($text)
-{
-    echo gettext($text);
+if (!function_exists('_e')) {
+    /**
+     * Echo the translation for a textstring
+     * @param string $text
+     */
+    function _e($text)
+    {
+        echo gettext($text);
+    }
 }
 
-/**
- * Return singular/plural translation for a textstring
- * @param string $msg1
- * @param string $msg2
- * @param int $n
- * @return string
- */
-function _n($msg1, $msg2, $n)
-{
-    return ngettext($msg1, $msg2, $n);
+if (!function_exists('_n')) {
+    /**
+     * Return singular/plural translation for a textstring
+     * @param string $msg1
+     * @param string $msg2
+     * @param int $n
+     * @return string
+     */
+    function _n($msg1, $msg2, $n)
+    {
+        return ngettext($msg1, $msg2, $n);
+    }
 }
